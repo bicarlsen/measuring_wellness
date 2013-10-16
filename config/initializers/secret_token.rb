20665d0d9f6f137b2e66f89674ac7c54d7d630e8
Brian Carlsen <carlsen.bri@gmail.com>
2013-10-16 15:34:43 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeasuringWellness::Application.config.secret_key_base = 'f0475348dee8dbc2a00e11a49d289bd30e6a00c1cfc92bec0f9b3417a935401b6afebbb283901641914386702ce2de537c6593c5d0d25ee8dcbf69200d360fb2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MeasuringWellness::Application.config.session_store :cookie_store, key: '_measuring_wellness_session'
