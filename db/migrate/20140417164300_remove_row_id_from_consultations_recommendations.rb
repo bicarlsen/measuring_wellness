790c22fd5cd2a5aa09d258e5acd608dd2e8293f1
Brian Carlsen <carlsen.bri@gmail.com>
2014-04-27 10:11:17 -0600

    Bug fix for migrations

--- db/migrate/20140417164300_remove_row_id_from_consultations_recommendations.rb
+++ db/migrate/20140417164300_remove_row_id_from_consultations_recommendations.rb
@@ -1,6 +1,6 @@
 class RemoveRowIdFromConsultationsRecommendations < ActiveRecord::Migration
-  def change
+	def change
   	rename_table :consutations_recommendations, :consultations_recommendations
-		remove_column :consultations_recommendations, :rowid
+		#remove_column :consultations_recommendations, :rowid
 	end
 end
